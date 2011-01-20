df5897501317f1719ed7217069c8a975393d5490
Jean-Christian Imbeault <jc.imbeault@gmail.com>
2011-01-20 22:04:49 +0900

    11: Allow HTML in user profile description section
    useroptions.class.php:
      Use HTMLPurifier to validate user submitted code. This means we
      need to include the whole HTMLPurifier library. Luckily it's OSS

--- code/htdocs/lib2/logic/useroptions.class.php
+++ code/htdocs/lib2/logic/useroptions.class.php
@@ -10,6 +10,7 @@
 global $opt;
 
 require_once($opt['rootpath'] . 'lib2/logic/class.inputfilter.php');
+require_once($opt['rootpath'] . 'htmlpurifier-4.2.0/library/HTMLPurifier.auto.php');
 
 class useroptions
 {
@@ -120,9 +121,36 @@ class useroptions
 
   function tidy_html_description($text)
   {
-    $myFilter = new InputFilter(InputFilter::$allowedTags, InputFilter::$allowedAttr, 0, 0, 1);
-    $the_html = $myFilter->process($text);
-    return $the_html;
+    $options = array("input-encoding" => "utf8", "output-encoding" => "utf8", "output-xhtml" => true, "doctype" => "omit", "show-body-only" => true, "char-encoding" => "utf8", "quote-ampersand" => true, "quote-nbsp" => true, "wrap" => 0);
+    $config = HTMLPurifier_Config::createDefault();
+    $cssDefinition = $config->getCSSDefinition();
+
+    $cssDefinition->info['position'] = new
+      HTMLPurifier_AttrDef_Enum(array('absolute', 'fixed', 'relative', 'static', 'inherit'), false);
+
+    $cssDefinition->info['left'] = new HTMLPurifier_AttrDef_CSS_Composite(array(
+      new HTMLPurifier_AttrDef_CSS_Length(),
+      new HTMLPurifier_AttrDef_CSS_Percentage()
+    ));
+
+    $cssDefinition->info['right'] = new HTMLPurifier_AttrDef_CSS_Composite(array(
+      new HTMLPurifier_AttrDef_CSS_Length(),
+      new HTMLPurifier_AttrDef_CSS_Percentage()
+    ));
+
+    $cssDefinition->info['top'] = new HTMLPurifier_AttrDef_CSS_Composite(array(
+      new HTMLPurifier_AttrDef_CSS_Length(),
+      new HTMLPurifier_AttrDef_CSS_Percentage()
+    ));
+
+    $cssDefinition->info['bottom'] = new HTMLPurifier_AttrDef_CSS_Composite(array(
+      new HTMLPurifier_AttrDef_CSS_Length(),
+      new HTMLPurifier_AttrDef_CSS_Percentage()
+    ));
+
+    $purifier = new HTMLPurifier($config);
+    $clean_html = $purifier->purify($text);
+    return $clean_html;
   }
 }
 ?>
