2a83115936d422da297183aedf03a041863cf8bb
sva-cacher <sva_cacher@hotmail.com>
2011-03-26 13:09:56 +0100

    Forgot a couple of files
    Cache descriptions in the current locale is used

--- code/htdocs/rss/newcaches.php
+++ code/htdocs/rss/newcaches.php
@@ -3,8 +3,10 @@
 $opt['rootpath'] = $_SERVER['DOCUMENT_ROOT'] . '/';
 require_once('../lib2/web.inc.php');
 
+global $opt;
+
 $translator = new Language_Translator();
-$generator = new rss_FeedGenerator(new rss_CachesFeedData($translator));
+$generator = new rss_FeedGenerator(new rss_CachesFeedData($translator, $opt['template']['locale']));
 
 $generator->outputFeed();
 
---
+++ code/lib/classes/Rss/LogItems.php
@@ -0,0 +1,37 @@
+<?php
+
+class Rss_LogItems
+{
+  public function getItems()
+  {
+    $rs = sql("SELECT cache_logs.cache_id AS cache_id,
+                cache_logs.id AS log_id,
+                cache_logs.type AS log_type,
+                cache_logs.date AS log_date,
+                cache_logs.last_modified AS last_modified,
+                cache_logs.text AS log_text,
+                cache_logs.text_html AS text_html,
+                caches.name AS rss_cache_name,
+                user.username AS rss_username,
+                user.user_id AS user_id
+              FROM ((cache_logs
+                INNER JOIN caches ON (caches.cache_id = cache_logs.cache_id))
+                INNER JOIN user ON (cache_logs.user_id = user.user_id))
+              WHERE `cache_logs`.`cache_id` = `caches`.`cache_id`
+                AND `caches`.`status` != 4
+                AND `caches`.`status` != 5
+                AND `caches`.`status` != 6" 
+                .$this->getAdditionalWhere().
+            " ORDER BY cache_logs.last_modified DESC
+              LIMIT 20");
+
+    return $rs;
+  }
+
+  protected function getAdditionalWhere()
+  {
+    return "";
+  }
+}
+
+?>
