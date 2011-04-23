89a002f7311e1d0bdf1b1c7f8c0ce1086223e743
sva-cacher <sva_cacher@hotmail.com>
2011-04-23 16:32:25 +0200

    date_activate is NULL on new caches
    The existing caches might have a valid date_activate.

--- code/lib/classes/Cache/Where.php
+++ code/lib/classes/Cache/Where.php
@@ -4,7 +4,7 @@ class Cache_Where
 {
 	public static function active()
 	{
-		return ' caches.status = ' . Cache_Status::Active . ' AND caches.date_activate <= NOW() ';
+		return ' caches.status = ' . Cache_Status::Active . ' AND (caches.date_activate IS NULL OR caches.date_activate <= NOW()) ';
 	}
 
 	public static function publishNow()
