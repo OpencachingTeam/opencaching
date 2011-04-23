ffa0841585f2dd04aa698f278ef0201dc2f21349
OlofL <olof_laurin@yahoo.com>
2011-04-23 21:31:19 +0200

    Revoke substitute without translation

--- code/htdocs/lib2/translate.class.php
+++ code/htdocs/lib2/translate.class.php
@@ -42,25 +42,6 @@ class translate
 			$message = $plural;
 		$search = $this->prepare_text($message);
 
-		// safe to use non-mb since asc('%')<127
-		if (strpos($search, "%") > 0)
-		{
-			// try to substitue variables instead of gettext
-
-			global $translationHandler;
-
-			$trans = $search;
-
-			$variables = array();
-			$language_lower = mb_strtolower($language);
-			$translationHandler->loadNodeTextFile($variables, $opt['logic']['node']['id'].'.txt', $language_lower);
-			$translationHandler->loadNodeTextFile($variables, $opt['logic']['node']['id'].'-'.$language_lower.'.txt', $language_lower);
-			$trans = $translationHandler->substitueVariables($variables, $language_lower, $trans);
-			
-			if ($trans != $search)
-				return $trans;
-		}
-
 		return gettext($search);
 	}
 
