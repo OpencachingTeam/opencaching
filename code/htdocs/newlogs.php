42d018f2449b9b067ecdc0ded3f671790567b456
Oliver <o.dietz@nexgo.de>
2011-05-14 21:39:42 +0200

    Translation of country names in newlogs.php

--- code/htdocs/newlogs.php
+++ code/htdocs/newlogs.php
@@ -24,7 +24,22 @@
 		else
 			$sqlOrderBy = '';
 		
-		$rsLogs = sql_slave("SELECT `countries`.`de` AS `country_name`, `cache_logs`.`id`, `cache_logs`.`date_created`, `caches`.`name` AS `cachename`, `caches`.`wp_oc`, `cache_logs`.`type`, `cacheloguser`.`user_id`, `cacheloguser`.`username` FROM &loglist INNER JOIN `cache_logs` ON &loglist.`id`=`cache_logs`.`id` INNER JOIN `caches` ON `cache_logs`.`cache_id`=`caches`.`cache_id` INNER JOIN `user` AS `cacheloguser` ON `cache_logs`.`user_id`=`cacheloguser`.`user_id` INNER JOIN `countries` ON `caches`.`country`=`countries`.`short` ORDER BY ".$sqlOrderBy."`cache_logs`.`date_created` DESC");
+		$rsLogs = sql_slave("SELECT IFNULL(`sys_trans_text`.`text`, `countries`.`name`) AS `country_name`, 
+		                            `cache_logs`.`id`, 
+																`cache_logs`.`date_created`, 
+																`caches`.`name` AS `cachename`, 
+																`caches`.`wp_oc`, 
+																`cache_logs`.`type`, 
+																`cacheloguser`.`user_id`, 
+																`cacheloguser`.`username` 
+													 FROM &loglist 
+										 INNER JOIN `cache_logs` ON &loglist.`id`=`cache_logs`.`id` 
+										 INNER JOIN `caches` ON `cache_logs`.`cache_id`=`caches`.`cache_id` 
+										 INNER JOIN `user` AS `cacheloguser` ON `cache_logs`.`user_id`=`cacheloguser`.`user_id` 
+										 INNER JOIN `countries` ON `caches`.`country`=`countries`.`short` 
+										  LEFT JOIN `sys_trans_text` ON `countries`.`trans_id`=`sys_trans_text`.`trans_id` AND `sys_trans_text`.`lang`='&1'
+										   ORDER BY " . $sqlOrderBy . "`cache_logs`.`date_created` DESC",
+											          $opt['template']['locale']);
 		while ($rLog = sql_fetch_assoc($rsLogs))
 		{
 			$newLogs[] = $rLog;
