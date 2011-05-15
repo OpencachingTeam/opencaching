f2a972b93bfa9a3569d072b07667c61142a15100
Oliver <o.dietz@nexgo.de>
2011-05-15 16:34:08 +0200

    removed special menu items of oc.de

--- code/htdocs/lang/de/ocstyle/main.tpl.php
+++ code/htdocs/lang/de/ocstyle/main.tpl.php
@@ -205,18 +205,6 @@
 <?php 
 					echo $menu2->getTopMenuHtml();
 ?>
-<?php
-if ($oc_nodeid==7)
-echo '<!--'
-?>
-<li><a href="http://blog.geocaching.de/" target="_blank">Teamblog</a></li>
-<li><a href="http://www.geoclub.de/viewforum.php?f=52" target="_blank">Forum</a></li>
-<li><a href="http://webchat.freenode.net/?nick=Gast&channels=opencaching.de&prompt=1" target="_blank">Chat/IRC</a></li>
-<li><a href="http://geokrety.org/index.php?lang=de_DE.UTF-8" target="_blank">Geokrety</a></li>
-<?php
-if ($oc_nodeid==7)
-echo '-->'
-?>
 					</ul>
 				</div>
 				<!-- Buffer after header -->
